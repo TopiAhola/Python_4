82313538ec761cf4fec515b083f6b1be14fba6c6
TopiAhola <topi.ahola@metropolia.fi>
2025-01-20 15:59:51 +0200

    Laitteisto 1 materiaalia

---
+++ Digitaalitekniikka/1. Potenssiin nolla.py
@@ -0,0 +1,20 @@
+from string import hexdigits
+
+print(2 ** 2)
+print(3 ** 3 )
+print(2 ** 0)
+
+
+stringi = "f7"
+luku = int(stringi, 16) #Muuttaa 16 pohjaisen luvin 10 pohjaiseksi.
+print(luku)
+
+string_2 = 6
+luku_2 = hex(string_2)
+print(luku_2)
+
+numpy
+matplotlib
+pandas
+scipy
+import pandas
