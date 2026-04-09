f9aa242cf838d60ffc400b35be2858d582556aff
TopiAhola <aholatos@gmail.com>
2026-04-09 20:18:04 +0300

    Tietorakenteet ja Algoritmit Chapter 5 bonus execise ei toimi

--- tietorakenteet_ja_algoritmit/5_trees/_bonus_delete_node.py
+++ tietorakenteet_ja_algoritmit/5_trees/_bonus_delete_node.py
@@ -65,6 +65,10 @@ class Tree():
         Returns:
         - The node that contains such data or None if data is not found
         """
+        if data.__class__ is not int:
+            data = data.data
+
+
         current = self._root_node
         while current:
             if current.data == data:
@@ -102,37 +106,85 @@ class Tree():
             elif nod._parent._right_child == nod:
                 nod._parent._right_child = child_node
 
+
         else:
             #set child as root node
             self._root_node = child_node
+
+
+
+    def find_minimum_node(self, node):
+        """
+        Returns the minimum value of the tree
+        """
+
+        current_node = node
+        while current_node:
+            if current_node._left_child is not None:
+                current_node = current_node._left_child
+            else:
+                return current_node
+
+        return None
 
 
     def delete_node(self, data):
         found_node = self._find(data)
 
         if found_node is None:
+            #print("Node not found")
             return
-
-        #save children ?
-        left_child = found_node._left_child
-        right_child = found_node._right_child
-
-        #remove this?
-        if found_node is self._root_node:
-            pass
 
         #if no children
         if found_node._left_child is None and found_node._right_child is None:
-            found_node = None
+            #print("Node with data", found_node.data, "no children")
+            del found_node
             return
 
         #if only 1 child
-        if (found_node._left_child is not None and found_node._right_child is None) or (found_node._left_child is None and found_node._right_child is not None):
+        elif (found_node._left_child is not None and found_node._right_child is None) or (found_node._left_child is None and found_node._right_child is not None):
+            #print("Node with data", found_node.data, "1 children:", found_node._left_child.data if found_node._left_child else None,  found_node._right_child.data if found_node._right_child else None)
             self._detach_node(found_node)
+            del found_node
             return
 
         #if 2 children
         elif found_node._left_child is not None and found_node._right_child is not None:
+            #print("Node with data", found_node.data, "2 children: ",found_node._left_child.data if found_node._left_child else None,  found_node._right_child.data if found_node._right_child else None)
+
+            successor = self.find_minimum_node(found_node._right_child)
+
+            #if successor is the right child
+            if found_node._right_child == successor:
+                successor.parent = found_node._parent if found_node._parent is not None else None
+                #successor cannot have left child, successors right child remains unmodified
+                successor._left_child = found_node._left_child
+                found_node = successor
+
+
+            else:
+                #successor cannot have left child so it can be detached
+                self._detach_node(successor)
+
+                #replace target with successor
+                successor._left_child = found_node._left_child
+                successor._right_child = found_node._right_child
+                successor._parent = found_node._parent if found_node._parent is not None else None
+
+                found_node = successor
+
+            #set root
+            if found_node == self._root_node:
+                self._root_node = successor
+
+
+            del found_node
+
+        else:
+            #print("Error?")
+            pass
+
+
 
 
 
@@ -149,3 +201,91 @@ if __name__ == '__main__':
     tree.insert(35)
     tree.delete_node(35)
     print(tree._find(tree._root_node.data))
+
+    tree = Tree()
+    tree.insert(50)
+    tree.insert(20)
+    tree.insert(70)
+    tree.insert(90)
+    tree.insert(10)
+    tree.insert(40)
+    tree.insert(30)
+    tree.insert(35)
+    tree.delete_node(30)
+    print(tree._find(tree._root_node.data))
+
+    tree = Tree()
+    tree.insert(50)
+    tree.insert(20)
+    tree.insert(70)
+    tree.insert(90)
+    tree.insert(10)
+    tree.insert(40)
+    tree.insert(30)
+    tree.insert(35)
+    tree.delete_node(40)
+    print(tree._find(tree._root_node.data))
+
+    tree = Tree()
+    tree.insert(50)
+    tree.insert(20)
+    tree.insert(70)
+    tree.insert(90)
+    tree.insert(10)
+    tree.insert(40)
+    tree.insert(30)
+    tree.insert(35)
+    tree.delete_node(20)
+    print(tree._find(tree._root_node.data))
+
+    tree = Tree()
+    tree.insert(50)
+    tree.insert(20)
+    tree.insert(70)
+    tree.insert(90)
+    tree.insert(10)
+    tree.insert(40)
+    tree.insert(30)
+    tree.insert(35)
+    tree.delete_node(50)
+    print(tree._find(tree._root_node.data))
+
+    tree = Tree()
+    tree.insert(50)
+    tree.insert(20)
+    tree.insert(70)
+    tree.insert(90)
+    tree.insert(10)
+    tree.insert(40)
+    tree.insert(30)
+    tree.insert(35)
+    tree.delete_node(50)
+    tree.delete_node(20)
+    tree.delete_node(70)
+    tree.delete_node(90)
+    tree.delete_node(10)
+    tree.delete_node(40)
+    tree.delete_node(30)
+    tree.delete_node(35)
+    print(tree._find(tree._root_node))
+
+    import re
+
+    tree = Tree()
+    tree.insert(50)
+    tree.insert(20)
+    tree.insert(70)
+    tree.insert(90)
+    tree.insert(10)
+    tree.insert(40)
+    tree.insert(30)
+    tree.insert(35)
+    tree.delete_node(0)
+    print(tree._find(tree._root_node.data))
+
+    target = re.compile(tree._find(tree._root_node.data).__str__())
+    if re.search(target,"50<20<10<><>#><40<30<><35<><>#>#><>#>#><70<><90<><>#>#>#"):
+        print("true")
+
+    if tree._find(tree._root_node.data).__str__() == "50<20<10<><>#><40<30<><35<><>#>#><>#>#><70<><90<><>#>#>#":
+        print("true")
