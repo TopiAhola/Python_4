3c7de2d76d73bc9decf7f573c07f91d97e5cb74e
Topi Ahola <topi.ahola@metropolia.fi>
2026-04-22 10:38:17 +0300

    Chapter 1 aloitettu

---
+++ tietorakenteet_ja_algoritmit/1/_2.py
@@ -0,0 +1,28 @@
+
+
+
+number1 = input()
+number2 = input()
+number1 = int(number1)
+number2 = int(number2)
+print(number1, "+", number2, "is", int(number1 + number2) )
+print(number1, "-", number2, "is", int(number1 - number2) )
+print(number1, "*", number2, "is", int(number1 * number2) )
+print(number1, "/", number2, "is", (number1 / number2) )
+print(number1, "%", number2, "is", int(number1 % number2) )
+print(number1, "^", number2, "is", int(number1 ** number2) )
+
+
+
+
+
+'''
+10
+2
+10 + 2 is 12
+10 - 2 is 8
+10 * 2 is 20
+10 / 2 is 5.0
+10 % 2 is 0
+10 ^ 2 is 100
+'''
