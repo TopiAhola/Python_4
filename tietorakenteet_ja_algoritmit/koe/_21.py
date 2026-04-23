8ce65c0a80774434dd9bad94e10ab499d5a53e69
TopiAhola <aholatos@gmail.com>
2026-04-23 23:30:02 +0300

    Tietorakenteet ja Algoritmit koetehtävät ...

--- tietorakenteet_ja_algoritmit/koe/_21.py
+++ tietorakenteet_ja_algoritmit/koe/_21.py
@@ -1,4 +1,4 @@
-def tower_of_hanoi(count, stacks=None, source=0, auxiliary=1, destination=2, moves=0):
+def tower_of_hanoi_working_but_wrong(count, stacks=None, source=0, auxiliary=1, destination=2, moves=0):
     if not stacks:
         stacks = [['ABCDEFGHIJKLMNOPQRSTUVWXYZ'[i] for i in range(count-1, -1, -1)], [], []]
         moves = 1
@@ -14,8 +14,8 @@ def tower_of_hanoi(count, stacks=None, source=0, auxiliary=1, destination=2, mov
             #move a top piece A -> B -> C -> A
             if moves % 2 == 1:
                 #assert stacks[ (oddStep + 1) % 3 ] < stacks[oddStep]
-                stacks[ (oddStep + 1) % 3 ].append( stacks[oddStep].pop() )
-                oddStep = (oddStep + 1) % 3
+                stacks[ (oddStep + 2) % 3 ].append( stacks[oddStep].pop() )
+                oddStep = (oddStep + 2) % 3
 
             #move another
             elif moves % 2 == 0:
@@ -55,7 +55,59 @@ def tower_of_hanoi(count, stacks=None, source=0, auxiliary=1, destination=2, mov
             moves = moves + 1
             print(stacks)
 
-    print(moves)
+    #print(moves)
+    return moves
+
+def tower_of_hanoi(count, stacks=None, source=0, auxiliary=1, destination=2, moves=0):
+    if not stacks:
+        stacks = [['ABCDEFGHIJKLMNOPQRSTUVWXYZ'[i] for i in range(count-1, -1, -1)], [], []]
+        moves = 1
+        print(stacks)
+    # COMPLETE FROM HERE
+        source = stacks[source]
+        auxiliary = stacks[auxiliary]
+        destination = stacks[destination]
+
+        oddStep = 0
+        while len(destination) < count:
+
+            #move a top piece A -> B -> C -> A
+            if moves % 2 == 1:
+                #assert stacks[ (oddStep + 1) % 3 ] < stacks[oddStep]
+                stacks[ (oddStep + 2) % 3 ].append( stacks[oddStep].pop() )
+                oddStep = (oddStep + 2) % 3
+
+            #move another
+            elif moves % 2 == 0:
+
+                for index in range(0, 3):
+                    first_stack = stacks[index]
+                    next_stack = stacks[(index+1)%3]
+
+                    if len(first_stack) > 0 and len(next_stack) > 0:
+                        if first_stack[len(first_stack)-1] > next_stack[len(next_stack)-1]:
+                            if len(next_stack) > 0 and next_stack[len(next_stack)-1] != 'A':
+                                first_stack.append( next_stack.pop() )
+
+                        elif first_stack[len(first_stack)-1] < next_stack[len(next_stack)-1]:
+                            if len(first_stack) > 0 and first_stack[len(first_stack)-1] != 'A':
+                                next_stack.append( first_stack.pop() )
+
+                    elif len(first_stack) > 0 and first_stack[len(first_stack)-1] == 'A':
+                        continue
+
+
+
+            else:
+                raise ValueError
+
+            moves = moves + 1
+            print(stacks)
+
+    #print(moves)
+    return moves
+
+
 
 
 
@@ -63,7 +115,3 @@ def tower_of_hanoi(count, stacks=None, source=0, auxiliary=1, destination=2, mov
 
 #main
 print(tower_of_hanoi(3))
-if 'A' > 'B':
-    print('A')
-else:
-    print('B')
