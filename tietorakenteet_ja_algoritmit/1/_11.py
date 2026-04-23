521a2cf20c25922ca46d49306a89daaf951646be
TopiAhola <aholatos@gmail.com>
2026-04-23 18:25:03 +0300

    Tietorakenteet ja Algoritmit Chapter 1 done

--- tietorakenteet_ja_algoritmit/1/_11.py
+++ tietorakenteet_ja_algoritmit/1/_11.py
@@ -1,10 +1,23 @@
 def combine_lists(list1, list2):
+    return_list = []
+
+    while list1 and list2:
+        if list1[0] <= list2[0]:
+            return_list.append(list1.pop(0))
+
+        elif list1[0] > list2[0]:
+            return_list.append(list2.pop(0))
+
+        else:
+            raise ValueError()
+
     while list1:
+        return_list.append(list1.pop(0))
 
+    while list2:
+        return_list.append(list2.pop(0))
 
-
-
-
+    return return_list
 
 
 #main
