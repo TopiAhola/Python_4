6f74d2d53d9530c648fc4ccc84e884ec49c20157
TopiAhola <aholatos@gmail.com>
2026-07-07 20:55:01 +0300

    keyboard and mouse events

--- gui_projekti/filereader.py
+++ gui_projekti/filereader.py
@@ -10,3 +10,12 @@ def read_binary_file(filename : str):
     except Exception:
         print("File not found")
         return None
+
+def read_file_string(filename: str):
+    try:
+        with open(filename) as f:
+            return f.read()
+
+    except Exception:
+        print("File not found")
+        return None
